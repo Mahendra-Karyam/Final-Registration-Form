0bafb39cd72dfe49fc318f1b0600fae3d6090c33
Mahendra Karyam <karyammahendra@gmail.com>
2025-06-01 21:18:43 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -261,7 +261,7 @@ function AfterLogin() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <h1 className="text-green-500 text-2xl font-bold">
-        {email} Logged in Successfully!
+        {name} Logged in Successfully!
       </h1>
       <nav className="mt-5">
         <Link to="/">
@@ -282,7 +282,7 @@ function AfterSignUp() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <h1 className="text-green-500 text-2xl font-bold">
-        {email} Signed Up Successfully!
+        {name} Signed Up Successfully!
       </h1>
       <nav className="mt-5">
         <Link to="/">
